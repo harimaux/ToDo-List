9eee489cd5effe23e850e1fc2e72dc051cbcfff8
harimaux <lukaszwitek20@googlemail.com>
2022-04-06 13:26:41 +0100

    1.0

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 const inputBtn = document.getElementById("inputBtn");
+const warningNote = document.querySelector(".warningNote");
 
 inputBtn.addEventListener("click", () => {
+  warningNote.style.visibility = "hidden";
+
   const userInput = document.getElementById("userInput").value;
   document.getElementById("userInput").value = "";
 
-  const notesUl = document.getElementById("notes");
-  notesUl.innerHTML += `<li>${userInput}</li>`;
+  if (userInput == "") {
+    warningNote.style.visibility = "visible";
+  } else {
+    const notesUl = document.getElementById("notes");
+    notesUl.innerHTML += `<li>${userInput}</li>`;
 
-  const allNotes = document.querySelectorAll(".notes li");
+    const allNotes = document.querySelectorAll(".notes li");
 
-  for (note of allNotes) {
-    note.addEventListener("click", (e) => {
-      e.target.style.textDecoration = "line-through";
-    });
+    for (note of allNotes) {
+      note.addEventListener("click", (e) => {
+        e.target.classList.toggle("lineThroughToggle");
+      });
 
-    note.addEventListener("dblclick", (e) => {
-      e.target.remove();
-    });
+      note.addEventListener("dblclick", (e) => {
+        e.target.remove();
+      });
+    }
   }
 });
